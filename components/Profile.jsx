a2481b1a5f79a47238250c00a4662d1d3b79682b
Mark <ajineer@outlook.com>
2023-12-19 11:21:41 -0700

    Updates

---
+++ components/Profile.jsx
@@ -0,0 +1,43 @@
+import Circle from './Circle'
+import { motion } from 'framer-motion'
+
+function Profile(){
+    
+    const techURL = [
+        '/Javascript.png',
+        '/React.png',
+        '/Tailwind.png',
+        '/Python.jpeg', 
+        '/Flask.png',  
+        '/JSON.jpeg',  
+        '/github.png', 
+        '/CSS.png',       
+        '/HTML.png'
+    ]
+    
+    return(
+        <div className="flex items-center justify-center rounded-full w-[300px] h-[300px] mb-[10rem] mr-auto ml-auto sm:m-auto">
+            <div className='z-10 absolute flex overflow-clip items-center border-2 border-white rounded-full h-[190px] w-[190px]'>
+                <img className='h-full w-full' src='../../public/profile_picture.jpeg'/>
+            </div>
+            {techURL.map((url, idx) => {
+                return (
+                    // <Circle key={idx} url={url} idx={idx} />
+                    <motion.div key={idx} className='flex z-0 bg-opacity-0 absolute items-center w-[260px] h-[260px] rounded-full'
+                        initial={{rotate: 0, opacity: 0}}
+                        animate={{rotate: -12*idx, opacity: 1}}
+                        transition={{duration: 0.5, delay: (idx+1)*0.5}}
+                    >
+                        <motion.img className='z-10 rounded-full' width={'23px'} height={'23px'} src={url}
+                            initial={{rotate: 0}}
+                            animate={{rotate: 12*idx}}
+                            transition={{duration: 0.5, delay: (idx+1)*0.5}}
+                        />
+                    </motion.div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Profile
---
+++ projectStore.js
@@ -0,0 +1,73 @@
+const data = {
+  
+    projects: [
+    {
+      id: 1,
+      name: 'Task Master',
+      date_started: 'June, 2023',
+      date_finished: 'October, 2023',
+      description: 'A user-friendly to-do list tracker for efficient task management.',
+      summary: "Structured a JSON object to store to-do lists and tasks. Implemented HTML for the frontend user interface, ensuring a responsive and accessible design. Incorporated a custom CSS stylesheet for a visually appealing and user-friendly interface. Employed well-structured JavaScript functions to facilitate comprehensive CRUD operations on both to-do lists and their associated tasks.",
+      image: '../../public/TaskMasterimage.png',
+      youtube: 'DqRZbit4Yfw',
+      github: 'https://github.com/ajineer/phase-1-project',
+      demo: 'https://youtu.be/DqRZbit4Yfw',
+      stack: [
+        '../../public/Javascript.png',
+        '../../public/HTML.png',
+        '../../public/CSS.png',
+        '../../public/JSON.jpeg',
+        ]
+    },
+    {
+      id: 2,
+      name: 'Prackr',
+      date_started: 'July, 2023',
+      date_finished: 'October, 2023',
+      description: 'helps users track their pantry items and create gorcery lists.',
+      summary: "Utilized JSON server to store pantry items and their attributes. Implemented the React framework for the frontend user interface. Incorporated a custom CSS stylesheet for styling and design. Included full CRUD operations on pantry items.",
+      image: '../../public/Prackrimage.png',
+      youtube: 'M2iw366dyro',
+      github: 'https://github.com/ajineer/Phase-2-Project',
+      demo: 'https://youtu.be/M2iw366dyro',
+      stack: [
+        '../../public/Javascript.png',
+        '../../public/React.png',
+        '../../public/Tailwind.png',
+        '../../public/JSON.jpeg',
+        ]
+    },
+    {
+      id: 3,
+      name: 'Planly',
+      date_started: 'September, 2023',
+      date_finished: 'October, 2023',
+      description: 'An app that tracks and schedules a users events with an interactive calendar and to-do lists.',
+      summary: "Designed an interactive calendar and to-do list app for event tracking and scheduling. Utilized React to create a dynamic and user-friendly frontend interface and integrated Tailwind CSS for streamlined styling of custom React components. Implemented a robust API using Flask and Flask-SQLAlchemy, supporting complex routes and CRUD operations. Developed JavaScript functions to facilitate communication between the frontend and backend APIs.",
+      image: '../../public/Planlyimage.png',
+      youtube: 'p5V-HUNbyqU',
+      github: 'https://github.com/ajineer/phase-5-project',
+      demo: 'https://youtu.be/p5V-HUNbyqU',
+      stack: [
+        '../../public/React.png',
+        '../../public/Tailwind.png',
+        '../../public/Flask.png',
+        '../../public/Python.jpeg',
+        ]
+    },
+  ],
+  summary: "I am a Full stack Web Developer, Recently graduated from Flatiron School. I have been trained in Javascript, React.js, and Python with Flask and OOP. I also possess a strong Mathematics background, Calculus I, II, and III, Pure Mathematics, Differential Equations, Linear Algebra. Currently I tutor algebra, trigonometry, and calculus across all grades and ages. I am also a disc-golf enthusiast and puzzle cube collector.",
+  techURL: ['/Javascript.png',
+  '/React.png',
+  '/Tailwind.png',
+  '/Python.jpeg', 
+  '/Flask.png',  
+  '/JSON.jpeg',  
+  '/github.png', 
+  '/CSS.png',       
+  '/HTML.png'],
+  profile: '../public/profile_picture.jpeg',
+  links: ['https://www.linkedin.com/in/mark-pierce-410326100/', 'https://github.com/ajineer', 'https://medium.com/@ajineer']
+}
+
+export default data
